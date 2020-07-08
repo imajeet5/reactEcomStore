acdd48e95cf14b8c0c2055be14503456d8e529d5
Ajeet Singh <imajeet5@gmail.com>
2020-07-08 22:30:44 +0530

    add style and some animation to the menu-items components

--- client/src/components/menu-item/menu-item.component.tsx
+++ client/src/components/menu-item/menu-item.component.tsx
@@ -12,12 +12,13 @@ const MenuItem: React.FunctionComponent<menuItemProps> = ({
   imageUrl,
   size,
 }) => (
-  <div
-    className={`${size} menu-item`}
-    style={{ backgroundImage: `url(${imageUrl})` }}
-  >
+  <div className={`${size} menu-item`}>
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    ></div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
